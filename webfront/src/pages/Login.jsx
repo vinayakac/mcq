28fb3883619642901a81de470a9b5cd733d897de
Shamanaik9980 <shyamalaeshwarnaik@gmail.com>
2024-10-03 16:39:25 +0530

    all done

--- webfront/src/pages/Login.jsx
+++ webfront/src/pages/Login.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-
-
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
